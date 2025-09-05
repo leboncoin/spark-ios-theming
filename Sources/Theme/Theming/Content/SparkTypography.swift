027bbbb04940108387e825925ac6c03e754b4c88
Robin Lemaire <robin.lemaire@adevinta.com>
2025-09-05 10:33:33 +0200

    [Fix] Swift 6 error

--- Sources/Theme/Theming/Content/SparkTypography.swift
+++ Sources/Theme/Theming/Content/SparkTypography.swift
@@ -14,84 +14,84 @@ public struct SparkTypography: Typography {
 
     // MARK: - Properties
 
-    public let display1: TypographyFontToken = TypographyFontTokenDefault(
+    public let display1: any TypographyFontToken = TypographyFontTokenDefault(
         size: 40,
         isHighlighted: true,
         textStyle: .largeTitle
     )
-    public let display2: TypographyFontToken = TypographyFontTokenDefault(
+    public let display2: any TypographyFontToken = TypographyFontTokenDefault(
         size: 32,
         isHighlighted: true,
         textStyle: .largeTitle
     )
-    public let display3: TypographyFontToken = TypographyFontTokenDefault(
+    public let display3: any TypographyFontToken = TypographyFontTokenDefault(
         size: 24,
         isHighlighted: true,
         textStyle: .largeTitle
     )
 
-    public let headline1: TypographyFontToken = TypographyFontTokenDefault(
+    public let headline1: any TypographyFontToken = TypographyFontTokenDefault(
         size: 20,
         isHighlighted: true,
         textStyle: .headline
     )
-    public let headline2: TypographyFontToken = TypographyFontTokenDefault(
+    public let headline2: any TypographyFontToken = TypographyFontTokenDefault(
         size: 18,
         isHighlighted: true,
         textStyle: .headline
     )
 
-    public let subhead: TypographyFontToken = TypographyFontTokenDefault(
+    public let subhead: any TypographyFontToken = TypographyFontTokenDefault(
         size: 16,
         isHighlighted: true,
         textStyle: .subheadline
     )
 
-    public let body1: TypographyFontToken = TypographyFontTokenDefault(
+    public let body1: any TypographyFontToken = TypographyFontTokenDefault(
         size: 16,
         isHighlighted: false,
         textStyle: .body
     )
-    public let body1Highlight: TypographyFontToken = TypographyFontTokenDefault(
+    public let body1Highlight: any TypographyFontToken = TypographyFontTokenDefault(
         size: 16,
         isHighlighted: true,
         textStyle: .body
     )
 
-    public let body2: TypographyFontToken = TypographyFontTokenDefault(
+    public let body2: any TypographyFontToken = TypographyFontTokenDefault(
         size: 14,
         isHighlighted: false,
         textStyle: .body
     )
-    public let body2Highlight: TypographyFontToken = TypographyFontTokenDefault(
+    public let body2Highlight: any TypographyFontToken = TypographyFontTokenDefault(
         size: 14,
         isHighlighted: true,
         textStyle: .body
     )
 
-    public let caption: TypographyFontToken = TypographyFontTokenDefault(
+    public let caption: any TypographyFontToken = TypographyFontTokenDefault(
         size: 12,
         isHighlighted: false,
         textStyle: .caption
     )
-    public let captionHighlight: TypographyFontToken = TypographyFontTokenDefault(
+    public let captionHighlight: any TypographyFontToken = TypographyFontTokenDefault(
         size: 12,
         isHighlighted: true,
         textStyle: .caption
     )
 
-    public let small: TypographyFontToken = TypographyFontTokenDefault(
+    public let small: any TypographyFontToken = TypographyFontTokenDefault(
         size: 10,
         isHighlighted: false,
         textStyle: .footnote
     )
-    public let smallHighlight: TypographyFontToken = TypographyFontTokenDefault(
+    public let smallHighlight: any TypographyFontToken = TypographyFontTokenDefault(
         size: 10,
         isHighlighted: true,
         textStyle: .footnote
     )
 
-    public let callout: TypographyFontToken = TypographyFontTokenDefault(
+    public let callout: any TypographyFontToken = TypographyFontTokenDefault(
         size: 16,
         isHighlighted: true,
         textStyle: .callout
